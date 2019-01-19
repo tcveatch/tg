ed2c53932418134cd0cf239f357fbdbbca8cc010
Tom Veatch <tcveatch@gmail.com>
2019-01-19 15:19:43 -0800

    Update tgio.js
    Refer to wiki

--- tgio.js
+++ tgio.js
@@ -1,5 +1,6 @@
 // read and write Translation Graphs
-
+// TGs are motivated and described in the wiki at github.com/tcveatch/tg
+//
 // functions returning TG 
 // read from, and write to
 // TGML, JSON, SQL
